27dd324e97661e4a42ad4d6cf981ce2ddef6c531
frankvega2010 <franco_vega@msn.com>
2018-11-26 12:38:24 -0300

    Added WIP .bat file to remove user config on git CMD

---
+++ removepehuen.bat
@@ -0,0 +1,9 @@
+:: It goes to your git folder and removes pehuen from the global config. You have to change the information in order
+:: to apply your access info.
+
+@echo off
+
+cd C:\Program Files\Git
+git config --global user.name frankvega2010
+git config --global user.email franco_vega@msn.com
+cd C:\Users\Administrador\Desktop\platformer-SFML
