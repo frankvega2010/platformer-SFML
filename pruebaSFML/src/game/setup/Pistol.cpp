d8cbb3e3a382867610078e93ca9690607d12faa7
frankvega2010 <franco_vega@msn.com>
2018-11-27 10:53:53 -0300

    Added WIP pistol class

---
+++ pruebaSFML/src/game/setup/Pistol.cpp
@@ -0,0 +1,14 @@
+#include "Pistol.h"
+
+namespace Game_Namespace
+{
+	Pistol::Pistol()
+	{
+	}
+
+
+	Pistol::~Pistol()
+	{
+	}
+
+}
---
+++ pruebaSFML/src/game/setup/Pistol.h
@@ -0,0 +1,16 @@
+#ifndef PISTOL_H
+#define PISTOL_H
+
+#include "weapons.h"
+
+namespace Game_Namespace
+{
+	class Pistol : public Weapons
+	{
+	public:
+		Pistol();
+		~Pistol();
+	};
+}
+
+#endif // PISTOL_H
